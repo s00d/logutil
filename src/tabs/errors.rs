9169eec80ca0191b06084e4498ac08dd8ca21529
Pavel Kuzmin <Virus191288@gmail.com>
2025-07-27 18:34:35 +0500

    fix(errors): update text color and add background color for header

--- src/tabs/errors.rs
+++ src/tabs/errors.rs
@@ -96,7 +96,8 @@ impl ErrorsTab {
         ])
         .style(
             Style::new()
-                .fg(Color::Rgb(0, 191, 255))
+                .fg(Color::Rgb(255, 255, 255))
+                .bg(Color::Rgb(80, 80, 80)) // Серый фон для заголовка
                 .add_modifier(Modifier::BOLD),
         );
 
